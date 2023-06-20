79d52371f8e4ca646db2623bc527c2d60726ebd1
MinYeol Yang <myyang@gdnps.com>
2023-06-20 18:08:44 +0900

    chapter06_2

---
+++ ch06/ch06_2/src/routes/Layout/NavigationBar.tsx
@@ -0,0 +1,14 @@
+import {Link} from '../../components'
+
+export default function NavigationBar() {
+  return (
+    <div className='flex p-4 bg-gray-200'>
+      <Link to="/" className='btn btn-link'>
+        Home
+      </Link>
+      <Link to="/board" className='btn btn-link ml-4'>
+        Board
+      </Link>
+    </div>
+  )
+}
